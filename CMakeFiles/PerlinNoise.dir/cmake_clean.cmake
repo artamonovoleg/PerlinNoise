ef3a804ae56e9811b9d1c2788fb5c96b7018b451
artamonovoleg <artamonovav2003@gmail.com>
2020-06-09 01:57:16 +0300

    cmake add

---
+++ CMakeFiles/PerlinNoise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PerlinNoise.dir/PPM/PPM.cpp.o"
+  "CMakeFiles/PerlinNoise.dir/PerlinNoise.cpp.o"
+  "CMakeFiles/PerlinNoise.dir/main.cpp.o"
+  "PerlinNoise"
+  "PerlinNoise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PerlinNoise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
