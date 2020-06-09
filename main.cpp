1357bf2d05674e2192e81dd9ead4057d9d8902fa
artamonovoleg <artamonovav2003@gmail.com>
2020-06-09 23:38:58 +0300

    50%

--- main.cpp
+++ main.cpp
@@ -11,18 +11,17 @@ int main()
     int height = 256;
     PPM image = PPM(width, height, "out.ppm");
     float heights [width] [height];
-    PerlinNoise noise = PerlinNoise(11);
+    PerlinNoise noise;
       
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
-            double j = (double)y/((double)width);
-			double i = (double)x/((double)height);
-			float n = noise.interpolateNoise3d(10 * i, 10 * j, 0.8);
-			n = 20 * noise.interpolateNoise3d(i, j, 0.8);
-			n = n - floor(n);
+            float y_p = (float) y / (float) width;
+			float x_p = (float) x / (float) height;
 
+            float n = noise.interpolateNoise3d(20 * x_p, 20 * y_p, 0.8);
+            n = n - floor(n);
             int rgb [] = {n * 255, n * 255, n * 255};
             image.drawPixel(x, y, rgb);
         }
